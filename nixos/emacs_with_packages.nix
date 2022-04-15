81ebe521250ea4c50844b2f092849cd54f0417cb
Louis Boulanger <louis.boulanger74@hotmail.fr>
2022-04-15 13:44:31 +0200

    Added flake for emacs

--- nixos/emacs_with_packages.nix
+++ nixos/emacs_with_packages.nix
@@ -1,19 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }: 
+{ pkgs }: 
 
 let emacs_with_packages =
 let
   myEmacs = pkgs.emacs;
   emacsWithPackages = (pkgs.emacsPackagesFor myEmacs).emacsWithPackages; 
 in
-  emacsWithPackages (epkgs: with epkgs.melpaStablePackages; [
+emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
     magit
     ivy
     pkgs.emacs27Packages.undo-tree
     projectile
-    flycheck
     company
     counsel
-  ]);
+]) ++ (with epkgs.melpaPackages; [
+    nix-mode
+    flycheck
+]));
   config = import ../emacs/emacs_config.nix { inherit pkgs; };
 in
   pkgs.writeShellScriptBin "emacs" ''
