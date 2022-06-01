7c2d7a5b715fbf17414d480816893e213bf67e64
Louis Boulanger <louis.boulanger74@hotmail.fr>
2022-06-01 15:25:12 +0200

    Added babel execution for R and latex

--- emacs/src/packages/org.el
+++ emacs/src/packages/org.el
@@ -8,3 +8,8 @@
 (define-key org-mode-map (kbd "<M-right>") nil)
 (setq org-link-elisp-skip-confirm-regexp "^\(magit-show-commit \\\"[a-f0-9]+\\\")$")
 (load-library "oc-basic")
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((R . t)
+   (latex . t)))
