8a23f143395ce7a6a3a60c8b22b7ace9deb169e3
Louis Boulanger <louis.boulanger74@hotmail.fr>
2022-07-25 16:20:51 +0200

    Added niw shell indicator in emacs

--- emacs/src/theme.el
+++ emacs/src/theme.el
@@ -22,17 +22,65 @@
 		    :weight 'normal
 		    :width 'normal)
 
+(defvar my/modeline-colors
+  (pcase-let ((`(,main-color ,main-color-indicator ,light-color ,light-color-indicator)
+	       (if (my/is-in-nix-shell)
+		   (list "#5A2987" "#7134A9" "#9B70C2" "#B899D4")
+		 (list "#222222" "#444444" "#333333" "#555555"))))
+    `(:main-color ,main-color
+      :main-color-indicator ,main-color-indicator
+      :light-color ,light-color
+      :light-color-indicator ,light-color-indicator)))
+
+(defface my/face-nix-indicator
+  `((t
+     :height 100
+     :box ,(list :color (plist-get my/modeline-colors :main-color-indicator) :line-width '(0 . 3))
+     :foreground "#ffffff"
+     :background ,(plist-get my/modeline-colors :main-color-indicator)
+     ))
+  "Face used to display the nix shell indicator in the modeline"
+  :group 'my/faces)
+
+(defface my/face-nix-indicator-inactive
+  `((t
+     :height 100
+     :box ,(list :color (plist-get my/modeline-colors :light-color-indicator) :line-width '(0 . 3))
+     :foreground "#ffffff"
+     :background ,(plist-get my/modeline-colors :light-color-indicator)
+     ))
+  "Face used to display the nix shell indicator in the inactive modeline"
+  :group 'my/faces)
+
 (set-face-attribute 'mode-line nil
 		    :height 100
-		    :box '(:color "#222222" :line-width 3)
+		    :box `(:color ,(plist-get my/modeline-colors :main-color) :line-width (3 . 3))
 		    :foreground "#ffffff"
-		    :background "#222222")
+		    :background (plist-get my/modeline-colors :main-color))
 
 (set-face-attribute 'mode-line-inactive nil
 		    :height 100
-		    :box '(:color "#333333" :line-width 3)
+		    :box `(:color ,(plist-get my/modeline-colors :light-color) :line-width (3 . 3))
 		    :foreground "#dddddd"
-		    :background "#333333")
+		    :background (plist-get my/modeline-colors :light-color))
+
+(let ((nix-status (if (my/is-in-nix-shell) " NIX-SHELL " " IMPURE ")))
+  (setq-default mode-line-format
+	(list
+	 `(:eval (propertize ,nix-status 'face 'my/face-nix-indicator))
+	 mode-line-front-space
+	 mode-line-mule-info
+	 mode-line-client
+	 mode-line-modified
+	 mode-line-remote
+	 mode-line-frame-identification
+	 mode-line-buffer-identification
+	 "   "
+	 mode-line-position
+	 "   "
+	 `(:propertize ("" mode-name))
+	 mode-line-end-spaces
+	 )))
 
 (set-face-attribute 'fringe nil
 		    :background "#000000")
